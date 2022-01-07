d5e5979421990843bfcbe0b7f3dea363deb40049
cesar-codes <cesar.avina@gmail.com>
2022-01-07 09:33:15 -0500

    add: daily kata  solution

---
+++ 8kyu/js/firstElems.js
@@ -0,0 +1,7 @@
+/* pick a set of first elements
+https://www.codewars.com/kata/572b77262bedd351e9000076
+Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n elements of the sequence.
+
+If n == 0 return an empty sequence []
+*/
+const first = (arr, n=1) => arr.slice(0, n)
