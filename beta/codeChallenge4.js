01401b492323f009e16ce398f999ffaa3a38f56e
cesar-codes <cesar.avina@gmail.com>
2022-01-14 11:22:55 -0500

    add: daily kata  solution

---
+++ beta/codeChallenge4.js
@@ -0,0 +1,13 @@
+/* All Star Code Challenge #4
+https://www.codewars.com/kata/586405a639c5abc2db0000e8
+Create a function that takes in an object of an nba player, and returns the college that the player went to
+
+var curry = {fname: "Steph", lname:"Curry", number:30, team:"Warriors", college:"Davidson"}
+getCollege(curry) // ==> "Davidson"
+  */
+
+function getCollege(player){
+    //code here
+      return player.college
+    }
+    
