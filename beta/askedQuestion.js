b47de55741c91148fa7646af4f921eaf335d1529
cesar-codes <cesar.avina@gmail.com>
2022-01-15 11:21:23 -0500

    add: daily kata  solution

---
+++ beta/askedQuestion.js
@@ -0,0 +1,16 @@
+/* The most asked question on CodeWars
+https://www.codewars.com/kata/5935ecef7705f9614500002d
+What is the most asked question on CodeWars?
+
+Can someone explain 
+
+You need to write a function detect that will check if a string starts with Can someone explain, case sensitive. Return true if so, false otherwise.
+
+Let's hope you don't write a solution that makes people ask that question at you!
+
+
+ */
+function detect(comment) {
+    return comment.startsWith('Can someone explain') ? true : false
+  }
+  
