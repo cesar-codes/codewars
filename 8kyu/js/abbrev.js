f5f0ba9604b6197f0ae11b7bfbc967f33b6ae9d7
cesar-codes <cesar.avina@gmail.com>
2022-02-03 15:14:07 -0500

    add: daily kata  solution

---
+++ 8kyu/js/abbrev.js
@@ -0,0 +1,19 @@
+/* Abbreviate a Two Word Name
+https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+ */
+function abbrevName(name){
+    name = name.split(' ')
+    let first = (name[0][0]).toUpperCase()
+    let last = (name[1][0]).toUpperCase()
+    return (first+'.'+last)
+  }
