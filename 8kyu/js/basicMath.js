fd32ebf3890f9da101edf2c33286461036555a37
cesar-codes <cesar.avina@gmail.com>
2022-02-04 11:44:31 -0500

    add: daily kata  solution

---
+++ 8kyu/js/basicMath.js
@@ -0,0 +1,20 @@
+/* Basic math operations
+https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation
+*/
+function basicOp(operation, value1, value2)
+{
+  switch(operation){
+      case '+': return value1 + value2;
+      break;
+      case '-': return value1 - value2;
+      break;
+      case '*': return value1 * value2;
+      break;
+      case '/': return value1 / value2;
+      break;
+  }
+}
