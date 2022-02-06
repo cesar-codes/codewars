617a8d35969961fb0ee01b621764a050185d6840
cesar-codes <cesar.avina@gmail.com>
2022-02-06 11:22:01 -0500

    add: daily kata  solution

---
+++ 8kyu/js/countingSheep.js
@@ -0,0 +1,15 @@
+/* Counting sheep
+https://www.codewars.com/kata/54edbc7200b811e956000556
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+*/
+const countSheeps = arrayOfSheep => arrayOfSheep.filter(Boolean).length
+
+/* 
+function countSheeps(arrayOfSheep) {
+  let counter = 0
+  for (let i = 0; i < arrayOfSheep.length; i++){
+    if (!arrayOfSheep[i]) counter++
+  }
+  return arrayOfSheep.length - counter
+}
+ */
