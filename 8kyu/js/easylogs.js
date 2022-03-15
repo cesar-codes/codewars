2801ff0e6d6c16c801e1deaf537bc896674d19df
cesar-codes <cesar.avina@gmail.com>
2022-03-15 18:23:54 -0400

    add daily coding challenge

---
+++ 8kyu/js/easylogs.js
@@ -0,0 +1,6 @@
+/* Easy Logs 
+https://www.codewars.com/kata/5b68c7029756802aa2000176
+Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+*/
+
+const logs = (x, a, b) => Math.log(a*b)/ Math.log(x)
