0f08328389e265e2ca1820439e03cdcdfabb7fd1
cesar-codes <cesar.avina@gmail.com>
2022-03-22 22:08:17 -0400

    cleanup

--- 8kyu/js/main.js
+++
@@ -1,9 +0,0 @@
-//https://www.codewars.com/kata/5265326f5fda8eb1160004c8
-// Convert a Number to a String!
-// We need a function that can transform a number into a string.
-// What ways of achieving this do you know?
-
-function numberToString(num) {
-    return num.toString()
-    // Return a string of the number here!
-  }
