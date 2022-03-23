684a7af175d5930d390133f1e587c75c36f01954
cesar-codes <cesar.avina@gmail.com>
2022-03-23 16:58:17 -0400

    adding a kata

---
+++ 8kyu/js/sumMixArray.js
@@ -0,0 +1,12 @@
+/* Sum Mixed Array
+https://www.codewars.com/kata/57eaeb9578748ff92a000009
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+*/
+function sumMix(x){
+    x = x.map(x => Number(x))
+    const y = x.reduce((adder, old ) => adder += old)
+    return y
+  }
