b16f77fc7513dc2ebf8fa1045eb3c28bd2881cff
cesar-codes <cesar.avina@gmail.com>
2022-04-01 12:15:16 -0400

    add daily katas

---
+++ 8kyu/js/lostWithoutMap.js
@@ -0,0 +1,12 @@
+/* Lost without a map
+https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+'function maps(x){
+    const cheat = x.map( i => i*2 )
+    return cheat;
+  }'
