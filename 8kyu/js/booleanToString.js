ca73541118594f17ad34f8e9b00026efc2671a91
cesar-codes <cesar.avina@gmail.com>
2022-04-06 19:24:35 -0400

    daily CW coding challenge

---
+++ 8kyu/js/booleanToString.js
@@ -0,0 +1,7 @@
+/* Cnvert a boolean to a string 
+https://www.codewars.com/kata/551b4501ac0447318f0009cd
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+const booleanToString = b => String(b)
