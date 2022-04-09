6167978d7b2fbabb91ddf29c94e89e423044e600
cesar-codes <cesar.avina@gmail.com>
2022-04-08 22:07:06 -0400

    Add daily coding challenge

---
+++ 8kyu/js/makeUpperCase.js
@@ -0,0 +1,6 @@
+/* Make Upper Case
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+Write a function which converts the input string to uppercase.
+*/
+
+const makeUpperCase = str => str.toUpperCase();
