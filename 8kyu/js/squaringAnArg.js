4429384bb1dab3c20630491c4ee4ea63f0c1d08a
cesar-codes <cesar.avina@gmail.com>
2022-04-10 20:43:00 -0400

    add daily codewars challenge

---
+++ 8kyu/js/squaringAnArg.js
@@ -0,0 +1,6 @@
+/* Squaring an argument
+https://www.codewars.com/kata/523b623152af8a30c6000027
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+// Write the "square"-function here
+const square = i => i*i
