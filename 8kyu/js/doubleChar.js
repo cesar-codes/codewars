86a3ae143857acec1ce7cdfb8c9ca99990083811
cesar-codes <cesar.avina@gmail.com>
2022-04-16 18:39:55 -0400

    add daily coding challenge

---
+++ 8kyu/js/doubleChar.js
@@ -0,0 +1,11 @@
+/* Doubler Char
+https://www.codewars.com/kata/56b1f01c247c01db92000076
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+*/
+const doubleChar = str => str.split('').map(a => a+=a).join('')
