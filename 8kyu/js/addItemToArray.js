5fa01cd55e3ba45d663dbab605d41085350da8fd
cesar-codes <cesar.avina@gmail.com>
2022-04-21 23:03:47 -0400

    add daily codewars challenge

---
+++ 8kyu/js/addItemToArray.js
@@ -0,0 +1,11 @@
+/* Basic Training: Add item to an Array
+https://www.codewars.com/kata/511f0fe64ae8683297000001
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+// add the value "codewars" to the already defined websites array
+websites.push('codewars')
