0285c1b502a3b49c491f32ff1f6e6c38c55f5e7f
cesar-codes <cesar.avina@gmail.com>
2022-04-28 14:50:19 -0400

    add daily coding challenge

---
+++ 8kyu/js/arrayMadness.js
@@ -0,0 +1,24 @@
+/* SpeedCode#2 - Array Madness
+https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1
+Objective
+Given two integer arrays a, b, both of length >= 1, create a program 
+that returns true if 
+the sum of the squares of each element in a 
+is strictly greater than 
+the sum of the cubes of each element in b.
+
+E.g.
+
+arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 
+4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+Get your timer out. Are you ready? Ready, get set, GO!!!
+*/
+let a = [4,5,6], b = [1,2,3]
+// square and sum a - cube and sum b
+const arrayMadness = (a,b) => {
+    let a1 = a.map(a => a*a).reduce((a,b)=> a+b)
+    let b1 = b.map(a => a**3).reduce((a,b)=>a+b)
+    console.log(a1 > b1 ? true : false)
+    return a1 > b1 ? true : false;
+}
+arrayMadness(a,b)
