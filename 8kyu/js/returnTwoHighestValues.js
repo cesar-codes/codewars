06db14245ce2bc13a18892b6841e2042862b8535
cesar-codes <cesar.avina@gmail.com>
2022-05-10 18:35:46 -0400

    add daily coding challenge solution

---
+++ 8kyu/js/returnTwoHighestValues.js
@@ -0,0 +1,21 @@
+/* Return the 2 highest Values in list
+https://www.codewars.com/kata/57ab3c09bb994429df000a4a
+In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  []
+ */
+
+
+function twoHighest(arr) {
+    let result = [...new Set(arr)]
+    if (result.length === 0) return []
+    else if (result.length === 1) return result
+    result = result.sort((a,b) => b - a, 0)
+    return [result[0], result[1]]
+  }
