e708c73a30a998733a57094972d031c4e711defb
cesar-codes <cesar.avina@gmail.com>
2022-05-20 10:50:10 -0400

    add coding challenge

---
+++ 8kyu/js/arrowFuncsOdd.js
@@ -0,0 +1,6 @@
+/* Are arrow functions odd?
+https://www.codewars.com/kata/559f80b87fa8512e3e0000f5
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+*/
+const odds = vals => vals.filter(a=> a % 2);
