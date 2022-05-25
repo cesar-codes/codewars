510c2b8897c5975423393a233cc8f22f60126c43
cesar-codes <cesar.avina@gmail.com>
2022-05-25 12:24:30 -0400

    add: daily coding challenge solution

---
+++ 8kyu/js/combineStrings.js
@@ -0,0 +1,6 @@
+/* Grasshopper cmbine strings
+https://www.codewars.com/kata/55f73f66d160f1f1db000059
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+ */
+const combineNames = (first, last) => `${first} ${last}`
