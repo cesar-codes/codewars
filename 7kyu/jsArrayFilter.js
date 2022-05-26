dade6c8923aae8244d91530fe07f90b744ed1c85
cesar-codes <cesar.avina@gmail.com>
2022-05-26 12:58:01 -0400

    add completed codewars challenge

---
+++ 7kyu/jsArrayFilter.js
@@ -0,0 +1,5 @@
+/* JavaScript Array Filter
+https://www.codewars.com/kata/514a6336889283a3d2000001
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+*/
+const getEvenNumbers = numbersArray => numbersArray.filter(a => a % 2 == 0)
