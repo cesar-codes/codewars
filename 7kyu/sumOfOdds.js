495f04425df92aa6ae651481cd844da9a7096d72
cesar-codes <cesar.avina@gmail.com>
2022-06-14 18:54:53 -0400

    add daily coding challenge

---
+++ 7kyu/sumOfOdds.js
@@ -0,0 +1,5 @@
+/* Sum of odd numbers
+https://www.codewars.com/kata/55fd2d567d94ac3bc9000064
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+*/
+const rowSumOddNumbers = n => Math.pow(n, 3)
