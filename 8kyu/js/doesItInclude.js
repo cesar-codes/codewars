5f8e0955f6a6c6fbab5bee6e0e1bf2124a27983a
cesar-codes <cesar.avina@gmail.com>
2022-06-19 19:06:34 -0400

    add daily coding challenge

---
+++ 8kyu/js/doesItInclude.js
@@ -0,0 +1,5 @@
+/* Enumerable Magic #3 - Does My List Include This?
+https://www.codewars.com/kata/545991b4cbae2a5fda000158
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false
+*/
+const include = (arr, item) => arr.includes(item)
