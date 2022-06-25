abb8ef13fef53fbc908679db25db51e72b5fba8f
cesar-codes <cesar.avina@gmail.com>
2022-06-25 16:39:02 -0400

    add daily coding challenge

---
+++ 7kyu/reverseLetter.js
@@ -0,0 +1,21 @@
+/* Reverse Letter
+https://www.codewars.com/kata/58b8c94b7df3f116eb00005b
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+
+*/
+function reverseLetter(str) {
+    str = str.replace(/[^a-z]/gi, '')
+    return str.split('').reverse().join('')
+    
+  }
