ecd180b18cfc09606df6222dd48b1ec402263d8d
cesar-codes <cesar.avina@gmail.com>
2022-07-21 15:41:00 -0400

    add: daily coding challenge solution

---
+++ 8kyu/js/thisIsAProblem.js
@@ -0,0 +1,11 @@
+/* 'this' is a problem
+https://www.codewars.com/kata/547c71fdc5b2b38db1000098
+We want to create a constructor function 'NameMe', which takes first name and last name as parameters. The function combines the first and last names and saves the value in "name" property.
+
+We already implemented that function, but when we actually run the code, the "name" property is accessible, but the "firstName" and "lastName" is not accessible. All the properties should be accessible. Can you find what's wrong with it? A test fixture is also available
+*/
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName  = last;
+    this.name = first + ' ' + last;
+}
