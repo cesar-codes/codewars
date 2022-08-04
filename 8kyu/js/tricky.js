158f4df6fae04b2ddd3eb6b301ddfcfd22949d4f
cesar-codes <cesar.avina@gmail.com>
2022-08-04 13:02:14 -0400

    add: daily coding challenge solution

--- 8kyu/js/tricky.js
+++ 8kyu/js/tricky.js
@@ -1,8 +1,27 @@
-/* 
+/* noobCode 02: TRICKY QUESTIONS ( primitives and operator precedence)
+https://www.codewars.com/kata/572269697297444c570008b9
+1 < 2 < 3 === true right? but could
+
+3 < 2 < 1 === true too??
+
+Here's your puzzle: Write a function greaterThanLessThan that takes three arguments, a, b, and c, and returns a boolean (true or false), such that:
+
+greaterThanLessThan(1, 2 3) === true
+and
+
+greaterThanLessThan(3, 2 1) === true
+But also
+
+graterThanLessThan(-3, -2, -1) == false
+with more examples on the expected behavior in the test cases.
+
+Hints
+Arguments do not necessarily have to be in strict ascending or descending order, as indicated by the example test cases.
+Check out the included test cases - not all arguments are going to return true! It's up to you to figure out which ones do and which ones don't.
+
+This challenge is a LOT easier than it might seem at first. Don't focus too much on building functions - just pay attention to precedence.
 
 */
-Given the integer n return odd numbers as they are, but subtract 1 from even numbers.
-
-Note:
-
-Your solution should be 22 or less characters long.
+function greaterThanLessThan (a,b,c) {
+    return a < b < c
+  }
