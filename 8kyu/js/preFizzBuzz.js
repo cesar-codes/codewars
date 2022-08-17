16437ba16f69f7e713f043cb45aa22b8ab53e59c
cesar-codes <cesar.avina@gmail.com>
2022-08-17 13:52:41 -0400

    add: daily codewars challenge solution

---
+++ 8kyu/js/preFizzBuzz.js
@@ -0,0 +1,10 @@
+/* Pre Fizz Buzz #1
+https://www.codewars.com/kata/569e09850a8e371ab200000b
+*/
+function preFizz(n) {
+    let result = []
+    for (let i = 1; i<n+1; i++){
+      result.push(i)
+    }
+    return result
+  }
