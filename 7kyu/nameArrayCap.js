f1d4a653a926bbcf7d68fcce7e5dea6f1a1d61d7
cesar-codes <cesar.avina@gmail.com>
2022-08-19 15:54:15 -0400

    add codewars challenge solution

---
+++ 7kyu/nameArrayCap.js
@@ -0,0 +1,8 @@
+/* Name Array Capping
+https://www.codewars.com/kata/5356ad2cbb858025d800111d
+Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+ */
+function capMe(names) {
+    return names.map(elem => elem.charAt(0).toUpperCase() + elem.slice(1).toLowerCase() )
+  }
+  
