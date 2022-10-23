4d61310e16bc7ec20510e5d50b29a7ad12a5bef0
cesar-codes <cesar.avina@gmail.com>
2022-10-23 16:19:13 -0400

    add daily coding challenge solution

---
+++ 7kyu/sumOfArrSingles.js
@@ -0,0 +1,16 @@
+/* Sum of Array Singles
+https://www.codewars.com/kata/59f11118a5e129e591000134
+In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+ */
+function repeats(arr){
+    console.log(arr)
+    let nonRepeats = []
+    for (let i = 0; i < arr.length; i++){
+      console.log(arr.indexOf(arr[i]), arr.lastIndexOf(arr[i]))
+      if ( arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i]) ) nonRepeats.push(arr[i])
+    }
+    return nonRepeats.reduce((a,b) => a + b, 0)
+  };
+  
