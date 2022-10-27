8055385c853aa17becc6522203b36f146e6e3ef2
cesar-codes <cesar.avina@gmail.com>
2022-10-27 13:16:21 -0400

    add daily coding challenge solution

---
+++ 6kyu/beginYourDay.js
@@ -0,0 +1,22 @@
+/* Begin your day with a challenge, but an easy one.
+https://www.codewars.com/kata/5873b2010565844b9100026d
+There are no explanations. You have to create the code that gives the following results in Python, Ruby, and Haskell:
+
+one_two_three(0) == [0, 0]
+one_two_three(1) == [1, 1]
+one_two_three(2) == [2, 11]
+one_two_three(3) == [3, 111]
+one_two_three(19) == [991, 1111111111111111111]
+And it should give the following results in Javascript, Scala, D, Go, and Rust:
+
+oneTwoThree(0) == ['0', '0']
+oneTwoThree(1) == ['1', '1']
+oneTwoThree(3) == ['3', '111']
+oneTwoThree(19) == ['991', '1111111111111111111']
+In C, the results are to be assigned to seperate pointers.
+ */
+
+function oneTwoThree(n) {
+    return ["9".repeat(n / 9) + (n % 9 ? n % 9 : "") || "0", "1".repeat(n) || "0"]
+  }
+
