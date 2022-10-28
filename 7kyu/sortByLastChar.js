3dcd1f3be0996599fd77e4e59093e6ff95173706
cesar-codes <cesar.avina@gmail.com>
2022-10-28 13:15:09 -0400

    add daily coding challenge solution

--- 7kyu/sortByLastChar.js
+++ 7kyu/sortByLastChar.js
@@ -9,6 +9,12 @@ All inputs will be valid.
 const last = x => x.split(' ').sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length -1))
 
 /* function last(x){
+  return x.split(' ').sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1) ) 
+}
+ */
+
+
+/* function last(x){
   console.log(x)
   x = x. split(' ')
   console.log(x[-1])
