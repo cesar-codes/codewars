1f38a517d46a4574fbb4150be2e8e896cb1fe98a
cesar-codes <cesar.avina@gmail.com>
2022-11-10 11:55:58 -0500

    add daily coding challenge solution

---
+++ 5kyu/movingZeros.js
@@ -0,0 +1,16 @@
+/* Moving zeroes to the end
+https://www.codewars.com/kata/52597aa56021e91c93000cb0
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+function moveZeros(arr) {
+    let zeros = arr.filter(n => n === 0).length
+    let newArr = arr.filter(n => n !== 0)
+    for (let i = 0; i < zeros; i++ )  {
+      newArr.push(0)
+    }
+    return newArr
+  }
+  
