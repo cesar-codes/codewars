79fc7ad7e35dc3d85b14ec9878c576e418469ec6
cesar-codes <cesar.avina@gmail.com>
2022-12-02 16:44:35 -0500

    add daily codewars challenge solution

--- 6kyu/breakCamelCase.js
+++ 6kyu/breakCamelCase.js
@@ -3,7 +3,16 @@ https://www.codewars.com/kata/5208f99aee097e6552000148
 Complete the solution so that the function will break up camel casing, using a space between words.
  */
 
+// using map and no regex 
+const solution = string => {
+    return string.split('')
+    // add a space before any upper cases and join
+      .map(letter => letter === letter.toUpperCase() ? ` ${letter}` : letter)
+      .join('')
+  }
+
+/* 
 // using regex
 function solution(string) {
     return string.split(/(?=[A-Z])/).join(' ')
-}
+} */
